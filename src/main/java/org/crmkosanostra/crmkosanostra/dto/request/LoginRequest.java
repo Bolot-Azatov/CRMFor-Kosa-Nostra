6bcb8c5f23416fea6c9db8e1e1ef93f843783c33
Bolot Azatov <bolotazatov23.05.03@gmail.com>
2026-08-17 15:33:50 +0600

    LoginRequest

---
+++ src/main/java/org/crmkosanostra/crmkosanostra/dto/request/LoginRequest.java
@@ -0,0 +1,12 @@
+package org.crmkosanostra.crmkosanostra.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    @NotBlank
+    private String username;
+    @NotBlank
+    private String password;
+}
