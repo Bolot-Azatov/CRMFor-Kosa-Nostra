95d2661b47eb5ccbe222b4421f554bac4fc48edc
Bolot Azatov <bolotazatov23.05.03@gmail.com>
2026-08-17 15:33:39 +0600

    Application

---
+++ src/main/java/org/crmkosanostra/crmkosanostra/CrmKosaNostraApplication.java
@@ -0,0 +1,13 @@
+package org.crmkosanostra.crmkosanostra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmKosaNostraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmKosaNostraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/crmkosanostra/crmkosanostra/CrmKosaNostraApplicationTests.java
@@ -0,0 +1,13 @@
+package org.crmkosanostra.crmkosanostra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmKosaNostraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
