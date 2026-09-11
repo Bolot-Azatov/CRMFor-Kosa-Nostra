d64a57758b4a9959af786b446881a5b0fb05c08d
Bolot Azatov <bolotazatov23.05.03@gmail.com>
2026-09-11 13:21:28 +0600

    Added a controller method that displays the profile of an authorized user

--- src/main/java/org/crmkosanostra/crmkosanostra/controller/ProfileController.java
+++ src/main/java/org/crmkosanostra/crmkosanostra/controller/ProfileController.java
@@ -2,7 +2,10 @@ package org.crmkosanostra.crmkosanostra.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.crmkosanostra.crmkosanostra.dto.response.UserResponse;
+import org.crmkosanostra.crmkosanostra.security.CustomUserDetails;
 import org.crmkosanostra.crmkosanostra.service.UserService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +18,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ProfileController {
 
     private final UserService userService;
+
+    @GetMapping()
+    private String showCurrentUserProfilePage(Model model, @AuthenticationPrincipal CustomUserDetails userDetails) {
+
+        UserResponse user = userService.findUserById(userDetails.getUserId());
+
+        model.addAttribute("user", user);
+        return "profile/view";
+    }
 
     @GetMapping("/{id}")
     private String showProfilePage(@PathVariable() Long id,
