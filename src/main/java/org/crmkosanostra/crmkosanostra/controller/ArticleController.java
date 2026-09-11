4e25bb671b252e15d798d2161334863129fb888f
Bolot Azatov <bolotazatov23.05.03@gmail.com>
2026-09-11 13:17:36 +0600

    Added a controller method for the route: articles/{id}

--- src/main/java/org/crmkosanostra/crmkosanostra/controller/ArticleController.java
+++ src/main/java/org/crmkosanostra/crmkosanostra/controller/ArticleController.java
@@ -5,6 +5,7 @@ import org.crmkosanostra.crmkosanostra.service.ArticleService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,5 +19,12 @@ public class ArticleController {
     public String getPublicArticles(Model model) {
         model.addAttribute("articles", articleService.getPublicArticles());
         return "public/articles";
+    }
+
+    @GetMapping("/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+
+        model.addAttribute("article", articleService.getArticleById(id));
+        return "public/article";
     }
 }
